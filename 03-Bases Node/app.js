b8518fe3eae66e4afdd78d22bb8da9df5840272c
Pablo Causa <causapablo@gmail.com>
2022-06-18 15:35:02 -0300

    Yargs

--- 03-Bases Node/app.js
+++ 03-Bases Node/app.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
 const { crearArchivoTabla } = require('./helpers/multiply')
+const argv = require('yargs').argv;
+const colors = require('colors');
 console.clear();
-const base = 17;
+/* 
+console.log("Process.argv",process.argv); */
 
-crearArchivoTabla(base)
+
+console.log("argv",argv);
+console.log("La base de yargs es : ", argv.base);
+crearArchivoTabla(argv.base)
     .then(archivo => console.log(archivo, "Created and saved Successfuly"))
     .catch(error => console.log(error));
